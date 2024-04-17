baea28a1806d382b4c6909cb52246cc383eebb70
hoangson <lehoangsonsg@gmail.com>
2024-04-18 09:08:26 +0930

    feat: update nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import {
   AiOutlineDoubleRight as RightIcon,
   AiOutlineDoubleLeft as LeftIcon,
 } from "react-icons/ai";
-import { useState} from "react";
+import { useState } from "react";
 import { styled } from "./components/context/factory";
 
 
@@ -10,13 +10,11 @@ function App() {
   const [visible, setVisible] = useState(false);
   const SLeftIcon = styled(LeftIcon)
   const SRightIcon = styled(RightIcon)
-  const Button = styled.button
-  
-  
+  const Button = styled("button")
+
+
   return (
-    <Button themeName={"toggleButton"} onClick={()=>setVisible(!visible)}>
-      {visible? <SLeftIcon themeName={"icons"}/>:<SRightIcon themeName={"icons"}/>}
-    </Button>
+    <Button themeName={"toggleButton"} onClick={() => setVisible(!visible)}>{visible ? <SLeftIcon themeName={"icons"} /> : <SRightIcon themeName={"icons"} />}</Button>
   )
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import '@radix-ui/themes/styles.css';
+import "./index.css"
 import { ThemeProvider } from './components/context/ThemeContext.tsx'
+import { Theme as RadixTheme } from '@radix-ui/themes';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <RadixTheme>
+        <App />
+      </RadixTheme>
     </ThemeProvider>
   </React.StrictMode>,
 )
