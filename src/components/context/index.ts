0ea0e23b522c040619d9cdfc8c520ecc76f28ba9
hoangson <lehoangsonsg@gmail.com>
2024-04-22 13:42:30 +0930

    feat: export default theme

--- src/components/context/index.ts
+++ src/components/context/index.ts
@@ -1,12 +1,14 @@
 import { styled } from "./factory";
 import { Theme, ThemeProvider, useThemeContext, PresetTheme } from "./ThemeContext";
 import { ITailwindKey, ITailwindTheme } from "./types";
+import { defaultTheme } from "./default";
 
 export {
     styled,
     Theme,
     ThemeProvider,
     useThemeContext,
+    defaultTheme
 }
 export type{
     ITailwindKey,
