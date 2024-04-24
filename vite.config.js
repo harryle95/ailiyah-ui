f39442fccdaccedd6c298a20008f6875218b51d5
hoangson <lehoangsonsg@gmail.com>
2024-04-24 19:16:22 +0930

    feat: add react testing library for testing

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    global: true,
+    environment: "jsdom",
+    setupFiles: "./setupTest"
+  },
   plugins: [react()],
 })
