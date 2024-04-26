7b0db006ad2541ed747acec1138602bfe2338ddf
harryle <lehoangsonsg@gmail.com>
2024-04-26 18:48:58 +0930

    feat: refactor, add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  framework: "@storybook/react-vite",
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
