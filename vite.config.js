c90602a7d80f977e692163c229f17e554ad32207
harryle <lehoangsonsg@gmail.com>
2024-05-02 08:32:14 +0930

    feat: update vite config to ignore certain folders

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
+    coverage: {
+      provider: "istanbul",
+      include: ["src/components/**"],
+      exclude: [
+        "**/*.stories.[jt]sx",
+        "**/*.helper.[jt]sx",
+        "**/*.helpers.[jt]sx",
+        "**/*.types.ts",
+        "src/components/accordion/**",
+        "src/components/dialog/**",
+        "src/components/navbar/**",
+        "src/components/button/**",
+      ],
+    },
     global: true,
     environment: "jsdom",
     setupFiles: "./setupTest",
     css: true,
   },
   plugins: [react()],
-})
+});
